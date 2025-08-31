b800bc5870203f0cd2605964e942644d76cdf356
Abdallah Yasser <a.yasser9999@yahoo.com>
2025-08-31 12:39:25 +0300

    feat(registration): Test Files

--- test/controllers/registrations_controller_test.rb
+++ test/controllers/registrations_controller_test.rb
@@ -11,17 +11,33 @@ class RegistrationsControllerTest < ActionDispatch::IntegrationTest
       assert_difference("Wizard.count", 1) do
         post registrations_url, params: {
           wizard: {
-            name: "Harry Potter",
-            email: "harry@hogwarts.com",
+            name: "Hogwarts User",
+            email: "user@hogwarts.com",
             password: "Magic123",
             date_of_birth: "1980-07-31",
-            bio: "The boy who lived.",
+            bio: "The boy who dead.",
             muggle_relatives: false
           }
         }
       end
     end
-    wizard = Wizard.last
-    assert_equal "Harry Potter", wizard.name
+  wizard = Wizard.last
+  assert_equal "Hogwarts User", wizard.name
+  assert_redirected_to landing_path
+  end
+  test "should not create wizard and redirect to registration page on validation error" do
+    assert_no_difference("Wizard.count") do
+      post registrations_url, params: {
+        wizard: {
+          name: "",
+          email: "invalid",
+          password: "123",
+          date_of_birth: "",
+          bio: "",
+          muggle_relatives: false
+        }
+      }
+    end
+    assert_redirected_to registrations_url, status: :unprocessable_content
   end
 end
